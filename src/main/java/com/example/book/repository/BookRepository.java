3f5158ec2b569faf76aae698c7c1229d0f36d12b
Sothuon_Thorn <sothuonthorn@gmail.com>
2018-06-26 15:08:24 +0700

    home

---
+++ src/main/java/com/example/book/repository/BookRepository.java
@@ -0,0 +1,15 @@
+package com.example.book.repository;
+
+import com.example.book.model.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookRepository {
+    List<Book> bookList = new ArrayList<>();
+    {
+        for (int i = 1; i <11 ; i++) {
+
+        }
+    }
+}
